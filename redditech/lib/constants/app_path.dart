026133027041dcdf4f134567788736df27beddfb
Carmona Maxime <maxime.carmona@epitech.eu>
2023-05-19 15:26:24 +0200

    Reddit-Flutter final

--- redditech/lib/constants/app_path.dart
+++
@@ -1,8 +0,0 @@
-class AppPath {
-  static const basePath = "/";
-  static const homeScreenPath = "/home";
-  static const subredditScreenPath = "$homeScreenPath/subreddit";
-  static const profileScreenPath = "/profile";
-  static const loginScreenPath = "/login";
-  static const searchScreenPath = "$homeScreenPath/search";
-}
